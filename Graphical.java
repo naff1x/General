dd57aa8e83eb3541e4115a5c241a8d25fd4181bb
naff1x <32478262+naff1x@users.noreply.github.com>
2018-06-10 20:36:22 +0200

    Added background image (file and code)

--- Graphical.java
+++ Graphical.java
@@ -5,37 +5,27 @@ import java.util.*;
 
 public class Graphical extends JFrame {
     private static final long serialVersionUID = 1L;
+    private ImageIcon image1;
+    private JLabel label1;
 
     JButton[] nines = new JButton[10];
     public void Window() { // Constructor. Creates window
         JFrame f = new JFrame("The Window");
-        f.setVisible(true);
+        f.setContentPane(new JLabel(new ImageIcon("500x500.jpg")));
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setResizable(false);
         f.setSize(500,500);
-        f.setLocationRelativeTo(null);
-        f.setLayout(new GridLayout(4,4,5,5));
+        f.setLocationRelativeTo(null);    
         
+        f.setLayout(new GridLayout(3,3,5,5));
         for (int i=1; i<10; i++) {
             nines[i] = new JButton("Number: " + i);
             f.add(nines[i]);
         }
-        /*
-        JButton button1 = new JButton("Hello");
-        f.add(button1);
 
-        JPanel p = new JPanel();
-        JButton button2 = new JButton("this is a panel");
-        p.add(button2);
-        f.add(p);
-        */
+        f.setVisible(true);
     }
-/*
-    public void panelAdder() {
-        JPanel p = new JPanel();
 
-    }
-*/
     public static void main(String[] args) { // Contains all the code and executions 
         Graphical g = new Graphical();
         g.Window();
