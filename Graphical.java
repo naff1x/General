43eb611e520207243c0164a85b589860240c07fa
naff1x <32478262+naff1x@users.noreply.github.com>
2018-06-11 20:01:30 +0200

    Added background image (background1.jpg) and title.

--- Graphical.java
+++ Graphical.java
@@ -3,8 +3,6 @@ import java.awt.*;
 import java.io.*;
 import java.util.*;
 
-//test
-
 public class Graphical extends JFrame {
     private static final long serialVersionUID = 1L;
     private ImageIcon image1;
@@ -12,14 +10,14 @@ public class Graphical extends JFrame {
 
     JButton[] nines = new JButton[10];
     public void Window() { // Constructor. Creates window
-        JFrame f = new JFrame("The Window");
-        f.setContentPane(new JLabel(new ImageIcon("500x500.jpg")));
+        JFrame f = new JFrame("Monster Feeder I");
+        f.setContentPane(new JLabel(new ImageIcon("background1.jpg")));
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setResizable(false);
-        f.setSize(500,500);
+        f.setSize(600,500);
         f.setLocationRelativeTo(null);    
         
-        f.setLayout(new GridLayout(3,3,7,7));
+        f.setLayout(new GridLayout(2,6,7,7));
         for (int i=1; i<10; i++) {
             nines[i] = new JButton("Number: " + i);
             f.add(nines[i]);
