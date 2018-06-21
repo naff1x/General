f75f1a9e206751534c59ddb2320a757242d42776
naff1x <32478262+naff1x@users.noreply.github.com>
2018-06-21 13:50:21 +0200

    Updated code. Added "pikachu.png"

--- GUI.java
+++ GUI.java
@@ -1,5 +1,7 @@
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.EventObject;
@@ -8,7 +10,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
 import java.io.*;
 
 
@@ -18,28 +19,39 @@ public class GUI extends JFrame implements ActionListener{
     
     private JButton button;
     private JLabel label;
+    private JPanel panel;
 
     public GUI(String name) {
         setTitle(name);
         setResizable(false);
         setContentPane(new JLabel(new ImageIcon("background1.jpg")));
-        setLayout(new FlowLayout());
+        //setLayout(new FlowLayout());
+        setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
         setSize(600,500);
         setVisible(true);
     }
 
     public void addComp() {
-        button = new JButton("Click me for text!");
+
+        Dimension buttonSize = new Dimension(120, 100);
+
+        button = new JButton(new ImageIcon("pikachu.png"));
+        //button = new JButton("Click me for text!");
+        button.setBounds(50, 50, 120, 100);
+        button.setContentAreaFilled(true);
+        button.setBorderPainted(false);
+        button.setFocusPainted(false);
+        button.setPreferredSize(buttonSize);
         add(button);
 
 
         label = new JLabel("");
+        label.setFont(new Font("Sans-Serif", Font.BOLD, 30));
+        label.setBounds(300, 10, 120, 100);
         add(label);
 
         button.addActionListener(this);
-        
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -49,8 +61,9 @@ public class GUI extends JFrame implements ActionListener{
     
 }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
         GUI mainGUI = new GUI("The GUI");
         mainGUI.addComp();
+        mainGUI.setLocationRelativeTo(null);
     }
 } // end of class "GUI"
