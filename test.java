bb6a1941048673720dd5fd235ce32e992fcb7d64
naff1x <naffixemail@gmail.com>
2018-07-03 14:01:41 +0200

    Added file test to check git status.

---
+++ test.java
@@ -0,0 +1,11 @@
+import java.io.PrintStream;
+
+public class test {
+
+    static PrintStream output = System.out;
+    public static void main(String[] args) {
+        int test = 0;
+        test++;
+        output.println("The number:" + test);
+    }
+}
