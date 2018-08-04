85dd7a3f3f5e1a2eec222a201d342bf51d942817
naff1x <naffixemail@gmail.com>
2018-08-04 17:14:02 +0200

    Basic JPanel for "Game.java" added.

--- Minesweeper/Game.java
+++ Minesweeper/Game.java
@@ -5,6 +5,7 @@ import java.io.*;
     import java.awt.FlowLayout;
     import java.awt.Font;
 import java.awt.FontFormatException;
+import java.awt.GridLayout;
 import java.awt.Insets;
     import java.awt.event.ActionEvent;
     import java.awt.event.ActionListener;
@@ -29,29 +30,48 @@ import org.w3c.dom.css.RGBColor;
 
 public class Game extends JFrame {
 
+    // TODO: Resize "bombIcon.png" and make it fit inside the 20x20 cells
+
     private static final long serialVersionUID = 1L;
 
     /// Variables for reciving input from constructor method
     private int bombs;
-    private int width;
-    private int height;
+    private int frameWidth;
+    private int frameHeight;
+    private int squareWidth;
+    private int squareHeight;
     /// Fonts
     private Font pixelFont;
-    /// 
-    
+    /// Remote variables for other classes
+    private Board playground;
+
     public Game(String nameFromInput, int widthFromInput, int heightFromInput, int bombsFromInput) {
         bombs = bombsFromInput;
-        width = widthFromInput * 20; // Multiplied by 20 to make room for the 20x20 cells that will make up the playing field
-        height = heightFromInput * 20;
+        frameWidth = widthFromInput * 20 + 40;   // Multiplied by 20 to make room for the 20x20 cells that will make up the playing field
+        frameHeight = heightFromInput * 20 + 50; // Plus 40 and 50 to add margin between the playing field and the border of the JFrame
+        squareWidth = widthFromInput;
+        squareHeight = heightFromInput;
 
         setTitle(nameFromInput);
         setResizable(false);
         setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setMinimumSize(new Dimension(200, 200));
-        setSize(width + 40, height + 50); // Plus 40 and 50 to add margin between the playing field and the border of the JFrame
+        setSize(frameWidth, frameHeight);
         setLocationRelativeTo(null);
         setVisible(true);
+        
+        /// Other methods and classes goes below
+        addFonts();
+        playground = new Board(squareWidth, squareHeight, frameWidth, frameHeight);
+        add(playground);
+
+        /* The code inside this comment box probably won't work as the variable "playground" (class Board) isn't applicable for the method "add"
+        playground = new Board();
+        playground.setBoard(widthFromInput, heightFromInput); // Passes on the width and height recived from "Game"-method's input arguments to the class "Board"
+
+        add(playground);
+        */
     } // end of contructor method "game"
 
     public void addFonts() {
@@ -67,8 +87,18 @@ public class Game extends JFrame {
     } // end of method "addFonts"
 } // end of class "game"
 
-class Cell extends JButton {
+class Board extends JPanel { 
 
-	private static final long serialVersionUID = 1L;
+    public Board(int xWidth, int yHeight, int frameWidth, int frameHeight) {
+        setLayout(new GridLayout(xWidth, yHeight));
+        setBackground(Color.yellow);
+        setBounds(10, 10, frameWidth - 20, frameHeight - 50);
+        setVisible(true);
+    } // end of constructor method "Board"
+} // end of class "Board"
+
+/*
+class Cell { // TODO: Should implement an ActionListner to push the logic down the hierarchy as far as possible.
 
 } // end of class "Cell" 
+*/
