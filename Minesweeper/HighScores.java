5f25dde6d5897da482622dac2f8c1c7d75d8f5a9
naff1x <naffixemail@gmail.com>
2018-09-28 16:56:35 +0200

    Updated background for "High Scores" (.png) and made minor code edits.

--- Minesweeper/HighScores.java
+++ Minesweeper/HighScores.java
@@ -44,12 +44,11 @@ public class HighScores extends JFrame {
     /// Other
         private ArrayList<Integer> diffcultyArray;
         private ArrayList<Integer> timeArray;
-        private ArrayList<ScoreEntry> top3Array;
     public HighScores(String name) {
         setTitle(name);
         setResizable(false);
         setLayout(null);
-        setContentPane(new JLabel(new ImageIcon("HighScores.png")));
+        setContentPane(new JLabel(new ImageIcon("HighScoresTemplate.png")));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(600,600);
         setLocationRelativeTo(null);
@@ -114,76 +113,76 @@ public class HighScores extends JFrame {
 
     public void rankScores() {
     
-    /// Create arrays for sorting values of difficulty and time
-    diffcultyArray = new ArrayList<Integer>();
-    timeArray = new ArrayList<Integer>();
+        /// Create arrays for sorting values of difficulty and time
+        diffcultyArray = new ArrayList<Integer>();
+        timeArray = new ArrayList<Integer>();
 
-    for (int i=0; i<scoreArray.size(); i++) { 
-        diffcultyArray.add(scoreArray.get(i).getDifficulty());
-        timeArray.add(scoreArray.get(i).getTime());
-    }
+        for (int i=0; i<scoreArray.size(); i++) { 
+            diffcultyArray.add(scoreArray.get(i).getDifficulty());
+            timeArray.add(scoreArray.get(i).getTime());
+        }
 
-    diffcultyArray.sort(Comparator.reverseOrder());
-    //Collections.sort(diffcultyArray);
-    output.println("-- DIFFICULTY: " + diffcultyArray);
+        diffcultyArray.sort(Comparator.reverseOrder());
+        //Collections.sort(diffcultyArray);
+        output.println("-- DIFFICULTY: " + diffcultyArray);
 
-    timeArray.sort(Comparator.naturalOrder());
-    output.println("-- TIME: " + timeArray);
+        timeArray.sort(Comparator.naturalOrder());
+        output.println("-- TIME: " + timeArray);
 
-    /// Loops and if-statements for ranking
+        /// Loops and if-statements for ranking
 
-        for (int i=0; i<scoreArray.size(); i++) { // Go through every object (score entry) [for every object..]
+            for (int i=0; i<scoreArray.size(); i++) { // Go through every object (score entry) [for every object..]
 
-            for (int d=0; d<diffcultyArray.size(); d++) { // [go through every difficulty added...]
+                for (int d=0; d<diffcultyArray.size(); d++) { // [go through every difficulty added...]
 
-                if (scoreArray.get(i).getDifficulty() == diffcultyArray.get(d)) { // If the current (i) scoreEntry's difficulty equals the current (d) item in difficultyArray...
+                    if (scoreArray.get(i).getDifficulty() == diffcultyArray.get(d)) { // If the current (i) scoreEntry's difficulty equals the current (d) item in difficultyArray...
 
-                    scoreArray.get(i).setRank(d); // Sets the current (i) scoreEntry's rank to ranking in "difficultyArray" (this will be adjusted later)
-                
-                    if (i > 0) { // If this IS NOT the first item in "scoreArray", proceed. [one object has already been given a rank])
+                        scoreArray.get(i).setRank(d); // Sets the current (i) scoreEntry's rank to ranking in "difficultyArray" (this will be adjusted later)
                     
-                        //output.println("s.A. item:" + i + " difficulty: " + scoreArray.get(i).getDifficulty() + " and s.A. i-1: " + scoreArray.get(i-1).getDifficulty());
+                        if (i > 0) { // If this IS NOT the first item in "scoreArray", proceed. [one object has already been given a rank])
+                        
+                            //output.println("s.A. item:" + i + " difficulty: " + scoreArray.get(i).getDifficulty() + " and s.A. i-1: " + scoreArray.get(i-1).getDifficulty());
 
-                        if (scoreArray.get(i).getDifficulty() == scoreArray.get(i-1).getDifficulty()) { // 
-    
-                            output.println("Difficulties matched!");
+                            if (scoreArray.get(i).getDifficulty() == scoreArray.get(i-1).getDifficulty()) { // 
+        
+                                output.println("Difficulties matched!");
 
-                            if (scoreArray.get(i).getTime() < scoreArray.get(i-1).getTime()) { // If current s.A. item has a lower time
-                                output.println("Current s.A. item has lower time!");
-                                scoreArray.get(i).decreaseRankVar();
-                            }
+                                if (scoreArray.get(i).getTime() < scoreArray.get(i-1).getTime()) { // If current s.A. item has a lower time
+                                    output.println("Current s.A. item has lower time!");
+                                    scoreArray.get(i).decreaseRankVar();
+                                }
 
-                            if (scoreArray.get(i).getTime() > scoreArray.get(i-1).getTime()) { // If current s.A. item has a higher time
-                                output.println("Current s.A. item has higher time!");
-                                scoreArray.get(i).increaseRankVar();
-                            }
-                        } 
+                                if (scoreArray.get(i).getTime() > scoreArray.get(i-1).getTime()) { // If current s.A. item has a higher time
+                                    output.println("Current s.A. item has higher time!");
+                                    scoreArray.get(i).increaseRankVar();
+                                }
+                            } 
+                        }
                     }
                 }
+
+                output.println("s.A. loop done! (" + i + ")");
             }
 
-            output.println("s.A. loop done! (" + i + ")");
+        for (int i=0; i<scoreArray.size(); i++) {
+        output.println("-- ENTRY (" + i + ") RANK: "+scoreArray.get(i).getRank());
+        output.println("-- ENTRY (" + i + ") DIFFICULTY: " + scoreArray.get(i).getDifficulty());
+        output.println("-- ENTRY (" + i + ") TIME: " + scoreArray.get(i).getTime());
         }
 
-    for (int i=0; i<scoreArray.size(); i++) {
-    output.println("-- ENTRY (" + i + ") RANK: "+scoreArray.get(i).getRank());
-    output.println("-- ENTRY (" + i + ") DIFFICULTY: " + scoreArray.get(i).getDifficulty());
-    output.println("-- ENTRY (" + i + ") TIME: " + scoreArray.get(i).getTime());
-    }
-
-    for (int i=0; i<scoreArray.size(); i++) {
-        
-        switch (scoreArray.get(i).getRank()) {
-            case 1:
-            output.println(scoreArray.get(i).getUsername() + " had the best score with " + scoreArray.get(i).getDifficulty() + "% !");
-                break;
-            case 2:
-            output.println(scoreArray.get(i).getUsername() + " had the 2nd best score with " + scoreArray.get(i).getDifficulty() + "% !");
-                break;
-            case 3:
-            output.println(scoreArray.get(i).getUsername() + " had the 3rd best score with " + scoreArray.get(i).getDifficulty() + "% !");
-                break;
+        for (int i=0; i<scoreArray.size(); i++) { // This loop finds the top 3 scores and prints out info.
+            
+            switch (scoreArray.get(i).getRank()) {
+                case 0:
+                output.println(scoreArray.get(i).getUsername() + " had the best score with " + scoreArray.get(i).getDifficulty() + "% !");
+                    break;
+                case 1:
+                output.println(scoreArray.get(i).getUsername() + " had the 2nd best score with " + scoreArray.get(i).getDifficulty() + "% !");
+                    break;
+                case 2:
+                output.println(scoreArray.get(i).getUsername() + " had the 3rd best score with " + scoreArray.get(i).getDifficulty() + "% !");
+                    break;
+            }
         }
-    }
     } // end of method "rankScores"
 } // end of class "game"
